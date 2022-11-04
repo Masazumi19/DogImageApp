09c4168b5f54497ab6cda010eaf16c0ab988b212
Masazumi19 <masakyrie0323@gmail.com>
2022-11-04 22:06:21 +0900

    Initial Commit

---
+++ DogImageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogImageApp
+//
+//  Created by 相川元澄 on 2022/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
